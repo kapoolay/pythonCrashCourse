7b9d73ce7b0fdc2ff4620eac013b8d7ac5f4951c
Kevin <>
2022-01-04 18:12:40 -0600

    sorting and reversing lists

---
+++ ch3_Introducing_Lists/cars.py
@@ -0,0 +1,45 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+# Sort cars in alpha order using .sort()
+# cars.sort()
+# print(cars)
+#
+# #Sort this list in reverse order using .sort(reverse=True) method
+# cars.sort(reverse=True)
+# print(cars)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Sort a List Temporarily with the .sorted() functions
+
+# print('Here is the original list:')
+# print(cars)
+#
+# #Sorting a list temporarily using sorted() method in the print function
+# print('\nHere is the sorted list:')
+# print(sorted(cars))
+#
+# print('\nHere is the original list again:')
+# print(cars)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Sort a List by reverse order using .reverse()
+
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# print(cars)
+#
+# #Reverse original order
+# cars.reverse()
+# print(cars)
+#
+# #Reverse the reversed order to the original
+# cars.reverse()
+# print(cars)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Finding the length of a list
+
+len(cars)
+print(len(cars))
