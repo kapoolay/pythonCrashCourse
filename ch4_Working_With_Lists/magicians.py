de2549acb4d2b35f5643685fb8148e633a991875
Kevin <>
2022-01-05 18:54:38 -0600

    chp 4 push

--- ch4_Working_With_Lists/magicians.py
+++ ch4_Working_With_Lists/magicians.py
@@ -1,4 +1,15 @@
 magicians = ['alice', 'david', 'carolina']
 
+# for magician in magicians:
+#     print(magician)
+
+# This can be read as 'For every magician in the list of magicians, print the magician's name'
+
+
+
+# Tell each magician in the list they performed great
+# Tell them we're looking forward to their next trip
+
 for magician in magicians:
-    print(magician)
+    print(f'{magician.title()}, that was great!')
+    print(f'I am looking forward to your next trip, {magician.title()}!\n')
