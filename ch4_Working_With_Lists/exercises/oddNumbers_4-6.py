44f61dc0ea2806a0b769cc95dd96a27f33ae16ae
Kevin <kapoolay@gmail.com>
2022-01-08 16:16:13 -0600

    ch4 exercises push

---
+++ ch4_Working_With_Lists/exercises/oddNumbers_4-6.py
@@ -0,0 +1,7 @@
+# Use the third argument of the range() to make a list of odd numbers 1 to 20
+# Use a for loop to print each number
+
+odd = range(1,20,2)
+
+for value in odd:
+    print(value)
---
+++ ch4_Working_With_Lists/exercises/oneMillion_4-4.py
@@ -0,0 +1,5 @@
+# Make a list of the numbers from one to one million.
+# Use a for loop to print the numbers.
+
+for value in range(1,1000001):
+    print(value)
