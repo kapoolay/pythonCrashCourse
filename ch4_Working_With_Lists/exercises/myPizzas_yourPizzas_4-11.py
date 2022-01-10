1fcd37edc47abe895e5c6189047a557a67c0682d
Kevin <kapoolay@gmail.com>
2022-01-09 18:24:21 -0600

    ch4 exercises push

---
+++ ch4_Working_With_Lists/exercises/myPizzas_yourPizzas_4-11.py
@@ -0,0 +1,22 @@
+# Start with exercise 4-1
+# Make a copy of the list of pizzas and call it friend_pizzas
+pizzas = ['pepperoni', 'pineapple', 'ham', 'cheese']
+friend_pizzas = pizzas[:]
+
+# Add a new pizza to the original list
+pizzas.append('bbq')
+
+# Add a different pizza to the list friend_pizzas
+friend_pizzas.append('olives')
+
+# Print the message 'My favorite pizzas are:'
+# Use a for loop to print the first list pizzas
+print('My favorite pizzas are:')
+for pizza in pizzas:
+    print(pizza)
+
+# Print the message 'My friend\'s favorite pizzas are:'
+# Use a for loop to pring the list friend_pizzas
+print('\nMy friend\'s favorite pizzas are:')
+for pizza in friend_pizzas:
+    print(pizza)
---
+++ ch4_Working_With_Lists/exercises/slices_4-10.py
@@ -0,0 +1,21 @@
+# Using one of the programs in this chapter, add several lines to the end of the program with the following:
+
+pizzas = ['pepperoni', 'pineapple', 'ham', 'cheese', 'olives', 'vegan']
+
+# Print the message 'The first three items in the list are:'.
+# Then use a slice to print the first three items from that program's list
+print('\nThe first three items in the list are:')
+for pizza in pizzas[:3]:
+    print(pizza)
+
+# Print the message 'Three items from the middle of the list are:'.
+# Use a slice to print three items from the middle of the list.
+print('\nThree items from the middle of the list are:')
+for pizza in pizzas[1:4]:
+    print(pizza)
+
+# Print the message 'The last three items in the list are:'.
+# Use a slice to print the last three items in the list.
+print('\nThe last three items in the list are:')
+for pizza in pizzas[:3]:
+    print(pizza)
