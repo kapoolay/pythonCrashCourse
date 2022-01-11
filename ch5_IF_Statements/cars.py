1e083e696700edc77bc2db4e84b8bd22d73ab6ba
Kevin <>
2022-01-11 17:26:46 -0600

    chp5 beginning

---
+++ ch5_IF_Statements/cars.py
@@ -0,0 +1,11 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+# Loops through the list of car names and looks for value 'bmw'. Print 'bmw' in uppercase instead of title case
+
+for car in cars:
+    # if the value is 'bmw' print in upper()
+    if car == 'bmw':
+        print(car.upper())
+    # else if the value is anything else, print in title()
+    else:
+        print(car.title())
