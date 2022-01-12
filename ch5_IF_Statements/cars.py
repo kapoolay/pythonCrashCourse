8ad443a527d6fcd658350259e0fef7f7e3d0898f
Kevin Capule <kapoolay@gmail.com>
2022-01-12 00:11:56 -0600

    review

--- ch5_IF_Statements/cars.py
+++ ch5_IF_Statements/cars.py
@@ -1,14 +1,14 @@
-# cars = ['audi', 'bmw', 'subaru', 'toyota']
-#
-# # Loops through the list of car names and looks for value 'bmw'. Print 'bmw' in uppercase instead of title case
-#
-# for car in cars:
-#     # if the value is 'bmw' print in upper()
-#     if car == 'bmw':
-#         print(car.upper())
-#     # else if the value is anything else, print in title()
-#     else:
-#         print(car.title())
+cars = ['audi', 'bMw', 'subaru', 'toyota']
+
+# Loops through the list of car names and looks for value 'bmw'. Print 'bmw' in uppercase instead of title case
+
+for car in cars:
+    # if the value is 'bmw' print in upper()
+    if car.lower() == 'bmw':
+        print(car.upper())
+    # else if the value is anything else, print in title()
+    else:
+        print(car.title())
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -20,21 +20,21 @@
 
 # A double equal sign (==) checks the value of the variable
 # "Is the value of car equal to 'bmw'?"
-car == 'bmw'
+# car == 'bmw'
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-# Case Checking for equality
-# Testing for equality is case sensitive in Python
-# 2 values with different capitalization are not considered equal
->>> car = 'Audi'
->>> car == 'audi'
-False
-
-# If case matters, then you're good
-# If case doesn't matter,  you can convert the variable's value to lowercase before doing the comparison:
->>> car = 'Audi'
->>> car.lower() == 'audi'
-True
->>> car
-'Audi'
+#
+# # Case Checking for equality
+# # Testing for equality is case sensitive in Python
+# # 2 values with different capitalization are not considered equal
+# >>> car = 'Audi'
+# >>> car == 'audi'
+# False
+#
+# # If case matters, then you're good
+# # If case doesn't matter,  you can convert the variable's value to lowercase before doing the comparison:
+# >>> car = 'Audi'
+# >>> car.lower() == 'audi'
+# True
+# >>> car
+# 'Audi'
--- ch5_IF_Statements/toppings.py
+++ ch5_IF_Statements/toppings.py
@@ -1,9 +1,13 @@
 # store a requested topping in a variable
 # print message if person did not order anchovies
 
-requested_topping = 'mushrooms'
+requested_topping = 'Mushroom'
 
 if requested_topping != 'anchovies':
     print('Hold the anchovies!')
 # If these 2 values do not match, execute the code following the IF statement
 # If the values do match, do not run the code
+
+# For fun
+if requested_topping.lower() == 'mushrooms':
+    print(f'Gimme my {requested_topping} pizza!')
