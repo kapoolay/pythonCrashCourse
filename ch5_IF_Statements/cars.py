4474f0e874d90a50ac48f69eace94960f36f35a8
Kevin Capule <kapoolay@gmail.com>
2022-01-11 18:04:35 -0600

    chp 5 push

--- ch5_IF_Statements/cars.py
+++ ch5_IF_Statements/cars.py
@@ -1,13 +1,40 @@
-cars = ['audi', 'bmw', 'subaru', 'toyota']
+# cars = ['audi', 'bmw', 'subaru', 'toyota']
+#
+# # Loops through the list of car names and looks for value 'bmw'. Print 'bmw' in uppercase instead of title case
+#
+# for car in cars:
+#     # if the value is 'bmw' print in upper()
+#     if car == 'bmw':
+#         print(car.upper())
+#     # else if the value is anything else, print in title()
+#     else:
+#         print(car.title())
 
-# Loops through the list of car names and looks for value 'bmw'. Print 'bmw' in uppercase instead of title case
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-for car in cars:
-    # if the value is 'bmw' print in upper()
-    if car == 'bmw':
-        print(car.upper())
-    # else if the value is anything else, print in title()
-    else:
-        print(car.title())
+# CONDITIONAL TESTS
 
-# repush
+# A single equal sign (=) sets the value of the variable
+# "Set the value of car equal to 'audi'"
+# car = 'bmw'
+
+# A double equal sign (==) checks the value of the variable
+# "Is the value of car equal to 'bmw'?"
+car == 'bmw'
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Case Checking for equality
+# Testing for equality is case sensitive in Python
+# 2 values with different capitalization are not considered equal
+>>> car = 'Audi'
+>>> car == 'audi'
+False
+
+# If case matters, then you're good
+# If case doesn't matter,  you can convert the variable's value to lowercase before doing the comparison:
+>>> car = 'Audi'
+>>> car.lower() == 'audi'
+True
+>>> car
+'Audi'
---
+++ ch5_IF_Statements/toppings.py
@@ -0,0 +1,9 @@
+# store a requested topping in a variable
+# print message if person did not order anchovies
+
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+    print('Hold the anchovies!')
+# If these 2 values do not match, execute the code following the IF statement
+# If the values do match, do not run the code
