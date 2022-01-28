6af36a495ff59e6d5b249c8596347aaabfd45764
Kevin Capule <kapoolay@gmail.com>
2022-01-27 23:32:32 -0600

    while loops push

--- ch7_User_Input_and_While_Loops/intro_while_loops/counting.py
+++ ch7_User_Input_and_While_Loops/intro_while_loops/counting.py
@@ -5,4 +5,10 @@ while currentNumber <= 5:
 	print(currentNumber)
 	# Add 1 to the currentNumber, then print again
 	currentNumber += 1
-	# This will keep repeating UNTIL currentNumber == 5
+	# "+=" operator is shorthand for currentNumber = currentNumber + 1
+
+# Python repeats the loop as long ast he condition "currentNumber <= 5" is true.
+# Bc 1 is less than 5, Python prints 1 and then adds 1, making the currentNumber == 2
+# Bc 2 is less than 5, Python prints 2 and adds 1 again, making the currentNumber == 3, and so on
+# Once the value of currentNumber is greater than 5, the loop stops running	
+
