6af845bb386775081a74028e4216dbb7b36bce88
Kevin Capule <kapoolay@gmail.com>
2022-02-05 22:29:04 -0600

    ch8 start

---
+++ ch8_Functions/greeter.py
@@ -0,0 +1,11 @@
+# Define a function
+def greetUser():
+    """Display a simple greeting."""
+    print("The purpose of this function is to print 'Hello!'")
+    print("\n\tHello!")
+
+# Call the function
+greetUser()
+
+# "def" is the keyword to inform Python that you're defining a function
+# Docstrings are enclosed in triple quotes, which describes what the function does
