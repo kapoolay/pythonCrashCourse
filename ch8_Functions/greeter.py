5bde315ee500a985a9bc528780b6aa62dac4ea0b
Kevin Capule <kapoolay@gmail.com>
2022-02-08 17:13:49 -0600

    ch7 push

--- ch8_Functions/greeter.py
+++ ch8_Functions/greeter.py
@@ -1,11 +1,22 @@
-# Define a function
-def greetUser():
+# # Define a function
+# def greetUser():
+#     """Display a simple greeting."""
+#     print("The purpose of this function is to print 'Hello!'")
+#     print("\n\tHello!")
+#
+# # Call the function
+# greetUser()
+#
+# # "def" is the keyword to inform Python that you're defining a function
+# # Docstrings are enclosed in triple quotes, which describes what the function does
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Pass info to a function
+def greetUser(username):
     """Display a simple greeting."""
-    print("The purpose of this function is to print 'Hello!'")
-    print("\n\tHello!")
+    print("\nThe purpose of this function is to print 'Hello!'")
+    print(f"\tHello {username.title()}!")
 
 # Call the function
-greetUser()
-
-# "def" is the keyword to inform Python that you're defining a function
-# Docstrings are enclosed in triple quotes, which describes what the function does
+greetUser("kevin")
+greetUser("alexia")
