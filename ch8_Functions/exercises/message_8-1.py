b8899685ca399b899aee822515ea7858ed306739
Kevin Capule <kapoolay@gmail.com>
2022-02-12 23:07:53 -0600

    ch8 exercises push

---
+++ ch8_Functions/exercises/message_8-1.py
@@ -0,0 +1,7 @@
+# Write a function called "display_message()" that prints one sentence telling everyone what you are learning about in this chapter
+# Call the function, and make sure the message displays correctly
+
+def displayMessage():
+    print("\nFor chapter 8, I am learning how to create functions along with being able to call that created function.")
+
+displayMessage()
