19cfc79b4e7248bdf76ac95b04ebd202e95bb170
Kevin Capule <kapoolay@gmail.com>
2022-03-08 20:23:49 -0600

    ch8 push

---
+++ ch8_Functions/person.py
@@ -0,0 +1,21 @@
+def build_person(firstName, lastName):
+    """Return a dictionary of information about a person"""
+    person = {'first': firstName, 'last': lastName}
+    return person
+
+musician = build_person('jimi', 'hendrix')
+print(musician)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Store a person's age as well
+
+def build_person2(firstName, lastName, age=None):
+    """Return a dictionary of information about a person"""
+    person = {'first': firstName, 'last': lastName}
+    if age:
+        person['age'] = age
+    return person
+
+musician2 = build_person2('kevin', 'capule', age=30)
+print(musician2)
+
